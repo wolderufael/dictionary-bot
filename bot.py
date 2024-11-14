473561ae4c0908f6b0ae4f7746089c979f780053
wolderufael <wolderuphaelkassahun@gmail.com>
2024-11-14 15:30:54 +0300

    dependencies

--- bot.py
+++ bot.py
@@ -1,88 +1,162 @@
-import os
+# import os
 
-from telegram.ext import Updater, MessageHandler, Filters
-from telegram.ext import CommandHandler
+# from telegram.ext import Updater, MessageHandler, filters
+# from telegram.ext import CommandHandler
+# from diction import get_info
+
+# telegram_bot_token = '7676463052:AAHbFgkFUDR5-Js7MfXsxlx39TIzIlvI430'
+
+# updater = Updater(token=telegram_bot_token, use_context=True)
+# dispatcher = updater.dispatcher
+
+
+# # set up the introductory statement for the bot when the /start command is invoked
+# def start(update, context):
+#     chat_id = update.effective_chat.id
+#     context.bot.send_message(chat_id=chat_id, text="Hello there. Provide any English word and I will give you a bunch "
+#                                                    "of information about it.")
+
+
+# # obtain the information of the word provided and format before presenting.
+# def get_word_info(update, context):
+#     # get the word info
+#     word_info = get_info(update.message.text)
+
+#     # If an invalid English word was provided by the user, return the custom response from get_info() and exit the
+#     # function
+#     if word_info.__class__ is str:
+#         update.message.reply_text(word_info)
+#         return
+
+#     # extract the word the user provided
+#     word = word_info['word']
+
+#     # extract the origin of the word
+#     origin = word_info['origin']
+#     meanings = '\n'
+
+#     synonyms = ''
+#     definition = ''
+#     example = ''
+#     antonyms = ''
+
+#     # a word may have several meanings. We'll use this counter to track each of the meanings provided from the response
+#     meaning_counter = 1
+
+#     for word_meaning in word_info['meanings']:
+#         meanings += 'Meaning ' + str(meaning_counter) + ':\n'
+
+#         for word_definition in word_meaning['definitions']:
+#             # extract the each of the definitions of the word
+#             definition = word_definition['definition']
+
+#             # extract each example for the respective definition
+#             if 'example' in word_definition:
+#                 example = word_definition['example']
+
+#             # extract the collection of synonyms for the word based on the definition
+#             for word_synonym in word_definition['synonyms']:
+#                 synonyms += word_synonym + ', '
+
+#             # extract the antonyms of the word based on the definition
+#             for word_antonym in word_definition['antonyms']:
+#                 antonyms += word_antonym + ', '
+
+#         meanings += 'Definition: ' + definition + '\n\n'
+#         meanings += 'Example: ' + example + '\n\n'
+#         meanings += 'Synonym: ' + synonyms + '\n\n'
+#         meanings += 'Antonym: ' + antonyms + '\n\n\n'
+
+#         meaning_counter += 1
+
+#     # format the data into a string
+#     message = f"Word: {word}\n\nOrigin: {origin}\n{meanings}"
+
+#     update.message.reply_text(message)
+
+
+# # run the start function when the user invokes the /start command
+# dispatcher.add_handler(CommandHandler("start", start))
+
+# # invoke the get_word_info function when the user sends a message
+# # that is not a command.
+# dispatcher.add_handler(MessageHandler(Filters.text, get_word_info))
+# updater.start_webhook(listen="0.0.0.0",
+#                       port=int(os.environ.get('PORT', 5000)),
+#                       url_path=telegram_bot_token,
+#                     #   webhook_url='https://dictionary-bot1.herokuapp.com/' + telegram_bot_token
+#                       webhook_url='https://dictionary-bot-pbld.onrender.com/' + telegram_bot_token
+#                       )
+
+
+import os
+from telegram import Update
+from telegram.ext import (
+    Application, CommandHandler, MessageHandler, ContextTypes, filters
+)
 from diction import get_info
 
 telegram_bot_token = '7676463052:AAHbFgkFUDR5-Js7MfXsxlx39TIzIlvI430'
 
-updater = Updater(token=telegram_bot_token, use_context=True)
-dispatcher = updater.dispatcher
+# Initialize the Application
+app = Application.builder().token(telegram_bot_token).build()
 
+# Introductory statement for the bot when the /start command is invoked
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(
+        "Hello there. Provide any English word and I will give you a bunch of information about it."
+    )
 
-# set up the introductory statement for the bot when the /start command is invoked
-def start(update, context):
-    chat_id = update.effective_chat.id
-    context.bot.send_message(chat_id=chat_id, text="Hello there. Provide any English word and I will give you a bunch "
-                                                   "of information about it.")
-
-
-# obtain the information of the word provided and format before presenting.
-def get_word_info(update, context):
+# Obtain the information of the word provided and format before presenting.
+async def get_word_info(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     # get the word info
     word_info = get_info(update.message.text)
 
-    # If an invalid English word was provided by the user, return the custom response from get_info() and exit the
-    # function
-    if word_info.__class__ is str:
-        update.message.reply_text(word_info)
+    # If an invalid English word was provided by the user, return the custom response from get_info() and exit
+    if isinstance(word_info, str):
+        await update.message.reply_text(word_info)
         return
 
-    # extract the word the user provided
+    # Extract the word information
     word = word_info['word']
-
-    # extract the origin of the word
     origin = word_info['origin']
     meanings = '\n'
-
     synonyms = ''
     definition = ''
     example = ''
     antonyms = ''
 
-    # a word may have several meanings. We'll use this counter to track each of the meanings provided from the response
+    # Counter to track each meaning
     meaning_counter = 1
-
     for word_meaning in word_info['meanings']:
-        meanings += 'Meaning ' + str(meaning_counter) + ':\n'
+        meanings += f'Meaning {meaning_counter}:\n'
 
         for word_definition in word_meaning['definitions']:
-            # extract the each of the definitions of the word
             definition = word_definition['definition']
+            example = word_definition.get('example', '')
 
-            # extract each example for the respective definition
-            if 'example' in word_definition:
-                example = word_definition['example']
+            synonyms = ', '.join(word_definition.get('synonyms', []))
+            antonyms = ', '.join(word_definition.get('antonyms', []))
 
-            # extract the collection of synonyms for the word based on the definition
-            for word_synonym in word_definition['synonyms']:
-                synonyms += word_synonym + ', '
-
-            # extract the antonyms of the word based on the definition
-            for word_antonym in word_definition['antonyms']:
-                antonyms += word_antonym + ', '
-
-        meanings += 'Definition: ' + definition + '\n\n'
-        meanings += 'Example: ' + example + '\n\n'
-        meanings += 'Synonym: ' + synonyms + '\n\n'
-        meanings += 'Antonym: ' + antonyms + '\n\n\n'
+            meanings += f'Definition: {definition}\n\n'
+            meanings += f'Example: {example}\n\n'
+            meanings += f'Synonym: {synonyms}\n\n'
+            meanings += f'Antonym: {antonyms}\n\n\n'
 
         meaning_counter += 1
 
-    # format the data into a string
+    # Format the data into a string
     message = f"Word: {word}\n\nOrigin: {origin}\n{meanings}"
+    await update.message.reply_text(message)
 
-    update.message.reply_text(message)
+# Add handlers
+app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, get_word_info))
 
-
-# run the start function when the user invokes the /start command
-dispatcher.add_handler(CommandHandler("start", start))
-
-# invoke the get_word_info function when the user sends a message
-# that is not a command.
-dispatcher.add_handler(MessageHandler(Filters.text, get_word_info))
-updater.start_webhook(listen="0.0.0.0",
-                      port=int(os.environ.get('PORT', 5000)),
-                      url_path=telegram_bot_token,
-                      webhook_url='https://dictionary-bot1.herokuapp.com/' + telegram_bot_token
-                      )
+# Run the webhook for the bot
+app.run_webhook(
+    listen="0.0.0.0",
+    port=int(os.environ.get('PORT', 5001)),
+    url_path=telegram_bot_token,
+    webhook_url=f'https://dictionary-bot-pbld.onrender.com/{telegram_bot_token}'
+)
